f4cd6cb6c7b8bdb1406d82ce70ec8033997bb858
KatherineJA <136206274+KatherineJA@users.noreply.github.com>
2026-05-11 17:34:19 -0600

    Procfile con los requirements y cambios en app.py preparando el backend para la subida al servidor

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from http.server import HTTPServer
 from control.router import Router
+import os
+
+PORT = int(os.environ.get("PORT", 5000))  # Railway inyecta PORT automáticamente
 
 if __name__ == "__main__":
-    servidor = HTTPServer(("0.0.0.0", 5000), Router)
-    print("Servidor CletaEats corriendo en http://localhost:5000")
-    servidor.serve_forever()
+    server = HTTPServer(("0.0.0.0", PORT), Router)
+    print(f"Servidor CletaEats corriendo en puerto {PORT}")
+    server.serve_forever()
